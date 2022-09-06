4fbf3809226f0bee781b4468967d64e442807fc7
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-09-06 19:15:29 +0200

    added video in TourSection
    added random video #click on .play-button

--- src/components/TourSection.vue
+++ src/components/TourSection.vue
@@ -3,8 +3,12 @@
     <div class="tour-top">
         <div class="container">
             <SectionHeader :header="tourHeaderData"/>
-            <a href="#" class="play-button mt-5"><i class="fa-solid fa-play"></i></a>
+            <span class="play-button mt-5" @click="showVideo"><i class="fa-solid fa-play"></i></span>
         </div>
+        <div class="video-wrapper" v-if="show == true" @click="closeVideo">
+            <iframe width="960" height="540" src="https://www.youtube.com/embed/4LrQ8V6offk?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        
     </div>
     
     
@@ -21,6 +25,7 @@ export default {
     },
     data() {
         return {
+            show: false,
             tourHeaderData: {
                 icon: 'icon-7.png',
                 title: 'tour our facilities',
@@ -28,6 +33,14 @@ export default {
             },
         }
     },
+    methods: {
+        showVideo() {
+            this.show = true;
+        },
+        closeVideo() {
+            this.show = false;
+        }
+    }
 }
 </script>
 
@@ -41,13 +54,15 @@ export default {
     background-image: url(../assets/images/paralax-bg-tour-facilities.jpg);
     background-size: cover;
     background-position: center;
+    position: relative;
 
-    a {
+    span {
         color: white;
 
         &:hover {
             color: white; //bootstrap override
             transform: scale(1.1);
+            cursor: pointer;
         }
 
     }
@@ -65,5 +80,34 @@ export default {
     i {
         font-size: 1.5rem;
     }
+}
+
+.video-wrapper {
+    position: fixed;
+    top:50%;
+    left:50%;
+    transform: translate(-50%, -50%);
+    height: 100%;
+    width: 100%;
+    z-index: 999;
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.close-video {
+    position: absolute;
+    top: -10%;
+    right: -10%;
+    background-color: #081425;
+    color: white;
+    height: 50px;
+    width: 50px;
+    font-size: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
 }
 </style>
